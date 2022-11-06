eea7f38de71909066a20939bd9b582a1b4e7be39
Андрей Шеин <spallhex@mail.ru>
2022-11-06 20:20:59 +0700

    Rewrote some code

--- RockPaperScissors/RockPaperScissors.cpp
+++ RockPaperScissors/RockPaperScissors.cpp
@@ -2,47 +2,61 @@
 #include <stdlib.h>
 #include <cstring>
 
-int main()
-{
-    std::string pChoice;
-    std::string cChoice;
-    std::string lib[3] = { "R", "P", "S" };
-
+char GetCompChoice() {
+    char lib[3] = { 'R', 'P', 'S' };
     srand(time(NULL));
-    cChoice = lib[rand() % 3];
+    return lib[rand() % 3];
+}
 
-    std::cout << "Welcome to RockPaperScissors game! \n";
-    std::cout << "Make a choice:\n\nRock (R)\nPaper (P)\nScissors (S)\n\n";
-
-    std::cout << "Your choice: ";
+char GetPlayerChoice() {
+    char pChoice;
     std::cin >> pChoice;
+    return pChoice;
+}
 
+void SelectWinner(char pChoice, char cChoice){
     if (pChoice == cChoice) {
         std::cout << "Computer chose: " << cChoice << "\n";
         std::cout << "Draw!";
     }
-    else if (pChoice == "R" && cChoice == "S") {
+    else if (pChoice == 'R' && cChoice == 'S') {
         std::cout << "Computer chose: " << cChoice << "\n";
         std::cout << "You Win!";
     }
-    else if (pChoice == "S" && cChoice == "R") {
+    else if (pChoice == 'S' && cChoice == 'R') {
         std::cout << "Computer chose: " << cChoice << "\n";
         std::cout << "You Lose!";
     }
-    else if (pChoice == "P" && cChoice == "R") {
+    else if (pChoice == 'P' && cChoice == 'R') {
         std::cout << "Computer chose: " << cChoice << "\n";
         std::cout << "You Win!";
     }
-    else if (pChoice == "R" && cChoice == "P") {
+    else if (pChoice == 'R' && cChoice == 'P') {
         std::cout << "Computer chose: " << cChoice << "\n";
         std::cout << "You Lose!";
     }
-    else if (pChoice == "S" && cChoice == "P") {
+    else if (pChoice == 'S' && cChoice == 'P') {
         std::cout << "Computer chose: " << cChoice << "\n";
         std::cout << "You Win!";
     }
-    else if (pChoice == "P" && cChoice == "S") {
+    else if (pChoice == 'P' && cChoice == 'S') {
         std::cout << "Computer chose: " << cChoice << "\n";
         std::cout << "You Lose!";
     }
+}
+
+int main()
+{
+    char pChoice;
+    char cChoice;
+
+    std::cout << "Welcome to RockPaperScissors game! \n";
+    std::cout << "Make a choice:\n\nRock (R)\nPaper (P)\nScissors (S)\n\n";
+
+    cChoice = GetCompChoice();
+
+    std::cout << "Your choice: ";
+    pChoice = GetPlayerChoice();
+
+    SelectWinner(pChoice, cChoice);
 }
